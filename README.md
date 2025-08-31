6e4859e3f32a865926f55c31fb0e30e69a5f0255
[piccolit] <[tpiccoli@frba.utn.edu.ar]>
2025-08-31 18:42:01 -0300

    Co-authored-by: Rama3024 <Rama3024@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# sistema-solar
+
+Orden propuesto por rama:
+
+ //sistema inicial
+
+    //vector`<cuerpo>` sistema1 = {tierra, jupiter};
+
+    //sistema que se va actualizando con las funciones
+
+    //vector`<cuerpo>` sistema2 = actualizarEstado(sistema1);
+
+    //una vez que se actualiza todo: sistema1 = sistema2
+
+    //sistema1 = sistema2;
--- fisicas.cpp
+++ fisicas.cpp
@@ -8,16 +8,19 @@ using namespace std;
 struct cuerpo{
 
     string id;
-    unsigned int masa;      //kg
-    unsigned int radio;     // km
+    double masa;      //kg
+    double radio;     // km
     //vectores de 3 elementos: uno por cada coo. en el espacio (x,y,z)
-    vector<float> posicion;      //
-    vector<float> velocidad;     //
-    vector<float> aceleracion;   //
+    vector<float> posicion = {0.0, 0.0, 0.0};      //UA
+    vector<float> velocidad = {0.0, 0.0, 0.0};     //UA/medio dia terrestre
+    vector<float> aceleracion = {0.0, 0.0, 0.0};   //UA/medio dia terrestre^2
 };
 
 //Se carga la base de datos de cuerpos celestes, por cada carga se agrega un cuerpo celeste mas
-void cargarCuerpoCeleste(string id, unsigned int masa, unsigned int radio, float xInicial, float yInicial, float zInicial, float velocidadX, float velocidadY, float velocidadZ, vector<cuerpo> sistema){
+void cargarCuerpoCeleste(string id, unsigned int masa, unsigned int radio, 
+                        float xInicial, float yInicial, float zInicial, 
+                        float velocidadX, float velocidadY, float velocidadZ, 
+                        vector<cuerpo> & sistema){
 
     cuerpo nuevoCuerpo;
 
@@ -33,9 +36,10 @@ void cargarCuerpoCeleste(string id, unsigned int masa, unsigned int radio, float
     nuevoCuerpo.velocidad[1] = velocidadY;
     nuevoCuerpo.velocidad[2] = velocidadZ;
 
-    sistema.push_back(nuevoCuerpo);
+    sistema.push_back(move(nuevoCuerpo));
 }
 
+//SOPORTE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 bool mismaPosicion(cuerpo cuerpo1, cuerpo cuerpo2){
     // Post: devuelve true si los dos cuerpos estan en la misma posicion
@@ -56,7 +60,7 @@ bool sonDistintos(const cuerpo & cuerpo1, const cuerpo & cuerpo2){
     return !(mismaPosicion(cuerpo1, cuerpo2) && masa && radio);
 }
 
-
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 vector<float> calcularFuerzaGravitatoria(cuerpo cuerpo1, cuerpo cuerpo2){
 
@@ -81,45 +85,89 @@ vector<float> calcularFuerzaGravitatoria(cuerpo cuerpo1, cuerpo cuerpo2){
 }
 
 
-
-vector<float> sumatoriaFuerzas(cuerpo cuerpo1, vector<cuerpo> sistema){
-    // Post: devuelve un vector con la aceleracion del cuerpo dado segun los cuerpos del sistema.
+void calcularAceleracion(cuerpo & cuerpo1, vector<cuerpo> & sistema){
+    // Post: devuelve un vector con la aceleracion del cuerpo dado segun la atraccion gravitatoria sufrida a partir de los demas cuerpos del sistema.
+    
+        if(sistema.size() < 2){
+        cout << "Error, no se puede calcular aceleracion si el cuerpo no esta en un sistema de al menos dos entidades." << endl;
+        return;
+    }
+    
     vector<float> aceleracion;
     
     for(int i=0; i<sistema.size(); i++){
         if(sonDistintos(sistema[i], cuerpo1)){
-            aceleracion[0] = aceleracion[0] + calcularFuerzaGravitatoria(sistema[i], cuerpo1)[0];
-            aceleracion[1] = aceleracion[1] + calcularFuerzaGravitatoria(sistema[i], cuerpo1)[1];
-            aceleracion[2] = aceleracion[2] + calcularFuerzaGravitatoria(sistema[i], cuerpo1)[2];
+            aceleracion[0] = aceleracion[0] + ((calcularFuerzaGravitatoria(sistema[i], cuerpo1)[0])/cuerpo1.masa);
+            aceleracion[1] = aceleracion[1] + ((calcularFuerzaGravitatoria(sistema[i], cuerpo1)[1])/cuerpo1.masa);
+            aceleracion[2] = aceleracion[2] + ((calcularFuerzaGravitatoria(sistema[i], cuerpo1)[2])/cuerpo1.masa);
         }
             
     }
+    cuerpo1.aceleracion = aceleracion;
+}
+
+void siguientePosicion(cuerpo cuerpo){
+    //Post: Recibe un cuerpo y modifica su posicion(t) a posicion(t+1)
+
+    //Esto tiene en cuenta que la velocidad este medida en las mismas unidades que el tiempo.
+    cuerpo.posicion[0] = cuerpo.posicion[0] + cuerpo.velocidad[0];
+    cuerpo.posicion[1] = cuerpo.posicion[1] + cuerpo.velocidad[1];
+    cuerpo.posicion[2] = cuerpo.posicion[2] + cuerpo.velocidad[2];
 
 }
 
 
+void siguienteVelocidad(cuerpo cuerpo){
+    //Post: //Post: Recibe un cuerpo y modifica su velocidad(t) a velocidad(t+1)
 
-
-vector<float> siguienteVelocidad(cuerpo cuerpo){
-
-    cuerpo.aceleracion = sumatoriaFuerzas(cuerpo, vectorCuerpos);
-
-
-    return velocidad_siguiente_t;
-}
-
-//La funcion principal. Actualiza el estado de un cuerpo por cada frame, cada frame senializa un t++.
-void actualizarEstado(cuerpo cuerpo){
-
-
+    //Esto tiene en cuenta que la velocidad este medida en las mismas unidades que el tiempo.
+    cuerpo.velocidad[0] = cuerpo.velocidad[0] + cuerpo.aceleracion[0];
+    cuerpo.velocidad[1] = cuerpo.velocidad[1] + cuerpo.aceleracion[1];
+    cuerpo.velocidad[2] = cuerpo.velocidad[2] + cuerpo.aceleracion[2];
 
 }
+
+
+void nuevoEstado(vector<cuerpo> sistemaSolar){
+    
+    for(int i=0; i<sistemaSolar.size(); i++)                
+        calcularAceleracion(sistemaSolar[i],sistemaSolar); 
+    for(int i=0; i<sistemaSolar.size(); i++)                    
+        siguientePosicion(sistemaSolar[i]);                     //Actualiza la posicion de todos los sistemas a su inmediata posicion
+      //Ahora con las nuevas posiciones, calculamos las aceleraciones provocadas por sus nuevas atracciones
+    for(int i=0; i<sistemaSolar.size(); i++)
+        siguienteVelocidad(sistemaSolar[i]);                    //Actualiza su velocidad actual segun su nueva aceleracion, la velocidad sera la que determine la proxima posicion
+}
+
+
 
 int main(){
 
-    vector<cuerpo> sistema = {};
+    vector<cuerpo> sistemaSolar = {};
+    vector<float> aceleracion;
 
-    cargarCuerpoCeleste();
+//1. Se cargan cuerpos en el estado inicial del sistema con posiciones y velocidades iniciales, las aceleraciones NO ESTAN INICIALIZADAS
 
-    return 0;
+//  cargarCuerpoCeleste(id, masa, radio, x, y, z, vx, vy, vz)
+
+    cargarCuerpoCeleste("Sol",1.989e30,700000,
+                        0,0,0,
+                        0,0,0,sistemaSolar);
+    cargarCuerpoCeleste("Tierra", 5.97e24, 6357,
+                        -0.94,0.33,0,
+                         100,200,0, sistemaSolar); //puse 100,200,300 como ejemplo
+
+
+//2. Se inicializan las aceleraciones iniciales de todos los planetas dado su posicion respecto al resto
+
+    for(int i = 0; i< sistemaSolar.size(); i++)
+        calcularAceleracion(sistemaSolar[i],sistemaSolar);
+    
+
+//3. Se van actualizando los planetas del sistema solar
+
+    while(true)
+        nuevoEstado(sistemaSolar);
+
+
 }
